2a9f153beddde3ae761162a557173a3acc074dab
Gaosong <shimaring@qq.com>
2022-10-27 19:17:47 +0800

    chore(dps):初始化项目依赖与项目结构
    初始化SpringBoot项目，引入基本maven依赖

---
+++ src/main/java/com/cucaqi/CucaqiApplication.java
@@ -0,0 +1,13 @@
+package com.cucaqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucaqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucaqiApplication.class, args);
+	}
+
+}
