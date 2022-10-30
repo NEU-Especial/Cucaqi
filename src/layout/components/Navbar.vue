a8d4c7f1ad7ee4e269e18dc2f8f9ca9bd7cbfc8b
Gaosong <shimaring@qq.com>
2022-10-30 20:17:43 +0800

    feat: 添加数据大屏静态界面
    添加数据大屏静态界面，添加工具类以及组件依赖

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -7,7 +7,7 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <img src="https://i0.hdslb.com/bfs/article/c510c6f5e8b1b6718226ae50b489c863fc312cdf.jpg" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
